ba29fb87408cd8c5217dbe4ca178d4cbafb22a0d
Diomede Mazzone <diomede.mazzone@gmail.com>
2020-10-26 16:59:34 +0100

    esempio-09

--- esempio-09.py
+++ esempio-09.py
@@ -9,12 +9,13 @@
 #   return PARAMETROOUTPUT
 
 
+import math
 
 def area_del_cerchio (raggio):
 
 # con import possiamo importare un modulo esterno fatto da tante funzioni che possono servirci
 # math è il modulo che contiene tutte le funzioni matematiche più complesse delle 4 operazioni
-    import math
+   
 
 # math.pi richiama il valore di pi greco contenuto in path
 # math.pow(raggio,2) richiama la funzione potenza, il due eleva al quadrato il primo parametro raggio      
@@ -24,10 +25,8 @@ def area_del_cerchio (raggio):
     return area
 
 
-
-
 r1 = float(input("Inserisci il raggio del primo cerchio: "))
 r2 = float(input("Inserisci il raggio del secondo cerchio:"))
-differenza = abs(area_del_cerchio(r1)-area_del_cerchio(r2))  #abs() restituisce il valore assoluto
+differenza =abs(area_del_cerchio(r1)-area_del_cerchio(r2))  #abs() restituisce il valore assoluto
 
 print("la differenza tra le due aree è: ", differenza)

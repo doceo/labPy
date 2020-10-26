9e0dd13f4ac8c4c7617ce01822f520bd972559f7
Diomede Mazzone <diomede.mazzone@gmail.com>
2020-10-26 09:13:28 +0100

    modificato 09

---
+++ esempio-09.py
@@ -0,0 +1,33 @@
+#possiamo raggruppare i nostri sottoalgoritmi in funzioni separate
+#così da ordinare meglio il codice ed eventualmente riutilizzare le nostre funzioni
+
+#sintassi: 
+#def NOMEFUNZIONE (PARAMETRO INPUT)
+#   istruzione
+#   istruzione
+#   istruzione
+#   return PARAMETROOUTPUT
+
+
+
+def area_del_cerchio (raggio):
+
+# con import possiamo importare un modulo esterno fatto da tante funzioni che possono servirci
+# math è il modulo che contiene tutte le funzioni matematiche più complesse delle 4 operazioni
+    import math
+
+# math.pi richiama il valore di pi greco contenuto in path
+# math.pow(raggio,2) richiama la funzione potenza, il due eleva al quadrato il primo parametro raggio      
+    area=math.pi*math.pow(raggio,2)  
+
+# return restituisce il valore di output alla funzione che l'ha chiamata    
+    return area
+
+
+
+
+r1 = float(input("Inserisci il raggio del primo cerchio: "))
+r2 = float(input("Inserisci il raggio del secondo cerchio:"))
+differenza = abs(area_del_cerchio(r1)-area_del_cerchio(r2))  #abs() restituisce il valore assoluto
+
+print("la differenza tra le due aree è: ", differenza)

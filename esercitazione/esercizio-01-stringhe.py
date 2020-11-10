ee6aa73bcc362cb7f5607e6d142ce5024786e704
Diomede Mazzone <diomede.mazzone@gmail.com>
2020-11-10 08:55:27 +0100

    esercizio stringa

---
+++ esercitazione/esercizio-01-stringhe.py
@@ -0,0 +1,15 @@
+# si modifichi il codice  affinchè
+# data una stringa stampi a video un carattere alla volta.
+
+stringa = "modifica il codice che ti suggerisco"
+
+print("stampo tutta la stringa", stringa)
+
+while i < len(frase):
+    # che istruzione manca?
+    i = i + 1
+
+
+# stampa a monitor la lunghezza della stringa STRINGA
+
+print(????)

f76ce4c6b311f5f0af7b2783290cda66edf716b5
Diomede Mazzone <diomede.mazzone@gmail.com>
2021-01-23 09:12:23 +0100

    tipi

---
+++ elementi_base/tipi.py
@@ -0,0 +1,23 @@
+#python3
+#tipi di variabili
+
+'''
+e commenti molto lunghi
+'''
+
+numero = 3
+stringa = "casa con virgolette"
+stringa2 = 'casa con apici'
+booleano = False
+reale = 3.14 #float
+
+
+print(numero,type(numero))
+
+print(stringa,type(stringa))
+print(stringa2,type(stringa2))
+print(booleano,type(booleano))
+print(reale,type(reale))
+
+
+print("moltiplico il ",numero,"per il ",reale,"ottengo ",numero*reale )

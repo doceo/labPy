2a26f0d5e6abf606f1a4ece08077a1b8f4e73135
Diomede Mazzone <diomede.mazzone@gmail.com>
2021-09-30 12:03:50 +0200

    attributi privati in retta

--- coniche/retta.py
+++ coniche/retta.py
@@ -2,24 +2,26 @@
 
 class retta:
     def __init__(self,a,b,c):
-        self.a = a
-        self.b = b
-        self.c = c
+
+    # un attributo privato si dichiara con il doppio underscore ( __ )    
+        self.__a = a  
+        self.__b = b
+        self.__c = c
 
     def equazione(self):
-        if(self.b>0 and self.c>0):
-            return f'{self.a}x + {self.b}y + {self.c} = 0'
-        elif(self.b<0 and self.c>0):
-            return f'{self.a}x {self.b}y + {self.c} = 0'
-        elif(self.b>0 and self.c<0):
-            return f'{self.a}x + {self.b}y {self.c} = 0'
+        if(self.__b>0 and self.__c>0):
+            return f'{self.__a}x + {self.__b}y + {self.__c} = 0'
+        elif(self.__b<0 and self.__c>0):
+            return f'{self.__a}x {self.__b}y + {self.__c} = 0'
+        elif(self.__b>0 and self.__c<0):
+            return f'{self.__a}x + {self.__b}y {self.__c} = 0'
         else:
-            return f'{self.a}x {self.b}y {self.c} = 0'
+            return f'{self.__a}x {self.__b}y {self.__c} = 0'
 
 
     def trovaY(self, x):
-        if self.b!=0:
-            return self.c/self.b*x 
+        if self.__b!=0:
+            return self.__c/self.__b*x 
 
     def coppie(self):
         punti = []
@@ -28,7 +30,14 @@ class retta:
             punti.append((i,self.trovaY(i)))
         return punti
 
+
+# inizio del programma chiamante
+
+
 r = retta(-3,-4,-6)
 
 print(r.equazione())
 print(r.coppie())
+
+# il metodo dir permette di conoscere tutti i metodi applicabili all'oggetto passato come parametro
+print(dir(r))

dace123e3519e475df040e29b44f703ab51b52b8
Diomede Mazzone <diomede.mazzone@gmail.com>
2022-01-07 18:19:00 +0100

    ordine

--- pgZero/prendialieno.py
+++ pgZero/prendialieno.py
@@ -2,21 +2,31 @@ from random import randint
 
 alien = Actor("alien")
 
+WIDTH = 800
+HEIGHT = 800
+
+msg = ""
+
 def draw():
     screen.clear()
+    screen.fill("red")
     alien.draw()
+    screen.draw.text(msg,topleft=(WIDTH/2-30,10), color="white" , fontsize=32)
 
 def place_alien():
-    alien.x = randint(10,800)
-    alien.y = randint(10,600)
-
+    alien.x = randint(10,WIDTH-10)
+    alien.y = randint(10,HEIGHT-10)
 
 def on_mouse_down(pos):
+    global msg
     if alien.collidepoint(pos):
-        print("Bel colpo!!")
+        msg ="Bel colpo!!"
+        print(msg)
+        
         place_alien()
     else:
-        print("Mancato!")
+        msg ="mancato!"
+        print(msg)
         quit()
 
 place_alien()

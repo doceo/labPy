8e8322b2dc556f574d86c2a1624c249177a32b1e
Diomede Mazzone <diomede.mazzone@gmail.com>
2022-04-21 10:16:48 +0200

    esempio di main

---
+++ elementi_base/main_esempio/main.py
@@ -0,0 +1,13 @@
+'''
+La funzione main è la funzione principale.
+
+Il file si deve chiamare main.py ed è il primo file "cercato" dall'interprete Python.
+
+
+
+'''
+
+
+if __name__ == '__main__': 
+    pass
+
---
+++ elementi_base/main_esempio/utils.py
@@ -0,0 +1,15 @@
+
+'''
+Questo file include tutte le funzioni che di supporto alla 
+funzione main, é possibile dichiararle senza implementarle 
+attraverso l'utilizzo dell parola chiave 'pass'.
+
+'''
+
+def inizializza():
+    pass
+
+
+if __name__ == '__main__': 
+    pass
+

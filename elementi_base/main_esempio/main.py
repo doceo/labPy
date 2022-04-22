28337a55cecb620f707e25d3646277d72b1f9158
Diomede Mazzone <diomede.mazzone@gmail.com>
2022-04-22 09:45:04 +0200

    main aggiornato

--- elementi_base/main_esempio/main.py
+++ elementi_base/main_esempio/main.py
@@ -3,11 +3,26 @@ La funzione main è la funzione principale.
 
 Il file si deve chiamare main.py ed è il primo file "cercato" dall'interprete Python.
 
-
+Tutte le istruzioni dell'algoritmo principale devono essere racchiuse in un blocco if.
+In tal modo le istruzioni vengono interpretate solo se il file è eseguito direttamente
+dall'interprete e non perchè non importato all'interno di un altro file.
 
 '''
 
 
+# importo i moduli creati e le funzioni di supporto che devono stare nella stessa cartella
+from utils import *
+from modulo_1 import *
+from modulo_2 import *
+
+
 if __name__ == '__main__': 
-    pass
+    
+    inizializzazione()
+    '''
+    eseguo quello che serve...
+
+    '''
+
+
 
---
+++ elementi_base/main_esempio/modulo_2.py
@@ -0,0 +1,13 @@
+'''
+Ogni modulo importerà i moduli di dipendenza, implementa le funzionalità o le classi
+Il blocco if serve per poter testare le funzionalità eseguendo il codice di test solo
+se eseguito il file direttamente. Lo stesso codice rimarrà inutilizzato quando viene
+importato il modulo nella funzione principale.
+'''
+
+
+
+
+if __name__ == '__main__': 
+    pass
+

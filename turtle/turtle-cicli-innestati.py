b5e99efe2a286f11aaece58c3299c19b3cd54a20
Diomede Mazzone <diomede.mazzone@gmail.com>
2024-12-18 10:42:10 +0100

    Update turtle-cicli-innestati.py

--- turtle/turtle-cicli-innestati.py
+++ turtle/turtle-cicli-innestati.py
@@ -1,13 +1,14 @@
 import turtle
 
 tarty = turtle.Turtle()
-tarty.speed('fast')
+tarty.speed('slow')
 
-for n in range(36):
-    for i in range(6):
+for i in range(36):
+ for j in range(6):
         tarty.forward(100)
         tarty.left(60)
     tarty.right(10)
+
 
 
 '''
@@ -24,5 +25,13 @@ for i in range(36):
 
 '''
 
+
+'''
+tarty.circle(100, 180)
+tarty.left(90)
+tarty.penup()
+
+tarty.goto(-100,100)
+'''
 
 wait = input("premi invio per chiudere.") 

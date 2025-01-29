d21cfab05c0899d324e05bcdd0a204edffc7d0cb
Diomede Mazzone <diomede.mazzone@gmail.com>
2025-01-29 11:55:32 +0100

    Create 2e-2425-1-statemant.py

---
+++ esercizi/2e-2425-1-statemant.py
@@ -0,0 +1,17 @@
+
+'''
+come si può prevenire l'errore di inserimento del valore di input?
+'''
+
+num = input("inserisci un numero: ")
+
+if num >= 90:
+    print("A 90-100")
+elif num >= 80:
+    print("b 80-90")
+elif num >= 70:
+    print("C 70-80")
+elif num >= 60:
+    print("D 60-70")
+else:
+    print("f")

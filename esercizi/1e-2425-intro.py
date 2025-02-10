9908347221d86598b13c4d55ba1981054dbd0ba2
Diomede Mazzone <diomede.mazzone@gmail.com>
2025-02-10 09:00:23 +0100

    Create 1e-2425-intro.py

---
+++ esercizi/1e-2425-intro.py
@@ -0,0 +1,15 @@
+numero = 123 #questo è un numero
+print(type(numero))
+name = input("scrivi il tuo nome: ")
+#print(name)
+surname = input("Scrivi il cognome: ")
+print(type(surname))
+
+print("Posso concatenerare le variabili in output")
+
+nomeCognome = name + " " + surname
+print(nomeCognome)
+print(type(nomeCognome))
+
+print("Un numero: ", numero)
+
